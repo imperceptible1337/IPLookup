d51ae8f263ed2bce6c75c126badeb1b6c3cac0fe
imperceptible <59235957+imperceptible1337@users.noreply.github.com>
2022-06-09 20:25:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,35 @@
+# libraries
 from colorama import Fore
 from requests import get
 import colorama
-colorama.init()
 import requests
+import argparse
 import json
 import os
 
-os.system('cls' if os.name=='nt' else 'clear')
+# initialize colorama
+colorama.init()
 
-print(f'          {Fore.YELLOW}< {Fore.WHITE}-------- {Fore.YELLOW}>')
-print(f'{Fore.YELLOW}< {Fore.WHITE}- {Fore.CYAN}Made by "{Fore.RED}nameless#2056{Fore.CYAN}" {Fore.WHITE}- {Fore.YELLOW}>')
-print(f'          {Fore.YELLOW}< {Fore.WHITE}-------- {Fore.YELLOW}>')
-print()
+# define arguments for argparse
+parser = argparse.ArgumentParser(description='Get basic information on an IP\nexample: main.py 1.1.1.1 or main.py m')
+parser.add_argument('ip_selection', type=str, help="Your target IP\nm for your IP")
+options = parser.parse_args()
 
-IP_SELECTION = input(f'{Fore.LIGHTBLUE_EX}[{Fore.WHITE}-{Fore.LIGHTBLUE_EX}]{Fore.CYAN} Enter an IP, or enter {Fore.WHITE}m{Fore.CYAN} for your IP {Fore.YELLOW}> {Fore.RED}')
-if IP_SELECTION != "m":
-    IP = IP_SELECTION
+ip_selection = options.ip_selection
+
+# set the target IP to the IP the user entered
+if ip_selection != "m":
+    ip = ip_selection
+# but if the user entered 'm', then set the target IP to their IP
 else:
-    YOUR_IP = get("https://api.ipify.org").text
-    IP = YOUR_IP
-URL = requests.get(f'http://ip-api.com/json/{IP}?fields=continent,continentCode,country,countryCode,region,regionName,city,zip,timezone,currency,isp,org,as,asname,reverse,mobile,proxy,hosting')
-DATA = json.loads(URL.text)
-DATA = json.dumps(DATA)
+    your_ip = get("https://api.ipify.org").text
+    ip = your_ip
 
-os.system('cls' if os.name=='nt' else 'clear')
+# get the information for the IP
+url = get(f'http://ip-api.com/json/{ip}?fields=continent,continentCode,country,countryCode,region,regionName,city,zip,timezone,currency,isp,org,as,asname,mobile,proxy,hosting')
+data = json.loads(url.text)
+data = json.dumps(data)
 
-print(f'          {Fore.YELLOW}< {Fore.WHITE}-------- {Fore.YELLOW}>')
-print(f'{Fore.YELLOW}< {Fore.WHITE}- {Fore.CYAN}Made by "{Fore.RED}nameless#2056{Fore.CYAN}" {Fore.WHITE}- {Fore.YELLOW}>')
-print(f'          {Fore.YELLOW}< {Fore.WHITE}-------- {Fore.YELLOW}>')
-print(f'{Fore.LIGHTBLUE_EX}[{Fore.WHITE}-{Fore.LIGHTBLUE_EX}]{Fore.CYAN} Query {Fore.YELLOW}> {Fore.GREEN} {IP}')
-print()
-print(DATA[2:-2].replace('", "', '\n').replace('": "', ' > ').replace(', "', '\n').replace('": ', ' > ').replace('continent > ', f'{Fore.LIGHTBLUE_EX}[{Fore.WHITE}-{Fore.LIGHTBLUE_EX}]{Fore.CYAN} Continent {Fore.YELLOW}> {Fore.GREEN}').replace('continentCode > ', f'{Fore.LIGHTBLUE_EX}[{Fore.WHITE}-{Fore.LIGHTBLUE_EX}]{Fore.CYAN} Continent Code {Fore.YELLOW}> {Fore.GREEN}').replace('country > ', f'{Fore.LIGHTBLUE_EX}[{Fore.WHITE}-{Fore.LIGHTBLUE_EX}]{Fore.CYAN} Country {Fore.YELLOW}> {Fore.GREEN}').replace('countryCode > ', f'{Fore.LIGHTBLUE_EX}[{Fore.WHITE}-{Fore.LIGHTBLUE_EX}]{Fore.CYAN} Country Code {Fore.YELLOW}> {Fore.GREEN}').replace('region > ', f'{Fore.LIGHTBLUE_EX}[{Fore.WHITE}-{Fore.LIGHTBLUE_EX}]{Fore.CYAN} Region {Fore.YELLOW}> {Fore.GREEN}').replace('regionName > ', f'{Fore.LIGHTBLUE_EX}[{Fore.WHITE}-{Fore.LIGHTBLUE_EX}]{Fore.CYAN} Region Name {Fore.YELLOW}> {Fore.GREEN}').replace('city > ', f'{Fore.LIGHTBLUE_EX}[{Fore.WHITE}-{Fore.LIGHTBLUE_EX}]{Fore.CYAN} City {Fore.YELLOW}> {Fore.GREEN}').replace('zip > ', f'{Fore.LIGHTBLUE_EX}[{Fore.WHITE}-{Fore.LIGHTBLUE_EX}]{Fore.CYAN} Zip Code {Fore.YELLOW}> {Fore.GREEN}').replace('timezone > ', f'{Fore.LIGHTBLUE_EX}[{Fore.WHITE}-{Fore.LIGHTBLUE_EX}]{Fore.CYAN} Time Zone {Fore.YELLOW}> {Fore.GREEN}').replace('currency > ', f'{Fore.LIGHTBLUE_EX}[{Fore.WHITE}-{Fore.LIGHTBLUE_EX}]{Fore.CYAN} Currency {Fore.YELLOW}> {Fore.GREEN}').replace('isp > ', f'{Fore.LIGHTBLUE_EX}[{Fore.WHITE}-{Fore.LIGHTBLUE_EX}]{Fore.CYAN} ISP {Fore.YELLOW}> {Fore.GREEN}').replace('org > ', f'{Fore.LIGHTBLUE_EX}[{Fore.WHITE}-{Fore.LIGHTBLUE_EX}]{Fore.CYAN} ORG {Fore.YELLOW}> {Fore.GREEN}').replace('as > ', f'{Fore.LIGHTBLUE_EX}[{Fore.WHITE}-{Fore.LIGHTBLUE_EX}]{Fore.CYAN} AS {Fore.YELLOW}> {Fore.GREEN}').replace('asname > ', f'{Fore.LIGHTBLUE_EX}[{Fore.WHITE}-{Fore.LIGHTBLUE_EX}]{Fore.CYAN} AS Name {Fore.YELLOW}> {Fore.GREEN}').replace('reverse > ', f'{Fore.LIGHTBLUE_EX}[{Fore.WHITE}-{Fore.LIGHTBLUE_EX}]{Fore.CYAN} Reverse {Fore.YELLOW}> {Fore.GREEN}').replace('mobile > ', f'{Fore.LIGHTBLUE_EX}[{Fore.WHITE}-{Fore.LIGHTBLUE_EX}]{Fore.CYAN} Mobile {Fore.YELLOW}> {Fore.GREEN}').replace('proxy', f'{Fore.LIGHTBLUE_EX}[{Fore.WHITE}-{Fore.LIGHTBLUE_EX}]{Fore.CYAN} Proxy {Fore.YELLOW}> {Fore.GREEN}').replace('hosting > ', f'{Fore.LIGHTBLUE_EX}[{Fore.WHITE}-{Fore.LIGHTBLUE_EX}]{Fore.CYAN} Hosting {Fore.YELLOW}> {Fore.GREEN}'))
+# print the information
+print(f'{Fore.CYAN}Query {Fore.YELLOW}> {Fore.GREEN} {ip}')
+print(data[2:-1].replace('", "', '\n').replace('": "', ' > ').replace(', "', '\n').replace('": ', ' > ').replace('continent > ', f'{Fore.CYAN}Continent {Fore.YELLOW}> {Fore.GREEN}').replace('continentCode > ', f'{Fore.CYAN}Continent Code {Fore.YELLOW}> {Fore.GREEN}').replace('country > ', f'{Fore.CYAN}Country {Fore.YELLOW}> {Fore.GREEN}').replace('countryCode > ', f'{Fore.CYAN}Country Code {Fore.YELLOW}> {Fore.GREEN}').replace('region > ', f'{Fore.CYAN}Region {Fore.YELLOW}> {Fore.GREEN}').replace('regionName > ', f'{Fore.CYAN}Region Name {Fore.YELLOW}> {Fore.GREEN}').replace('city > ', f'{Fore.CYAN}City {Fore.YELLOW}> {Fore.GREEN}').replace('zip > ', f'{Fore.CYAN}Zip Code {Fore.YELLOW}> {Fore.GREEN}').replace('timezone > ', f'{Fore.CYAN}Time Zone {Fore.YELLOW}> {Fore.GREEN}').replace('currency > ', f'{Fore.CYAN}Currency {Fore.YELLOW}> {Fore.GREEN}').replace('isp > ', f'{Fore.CYAN}ISP {Fore.YELLOW}> {Fore.GREEN}').replace('org > ', f'{Fore.CYAN}ORG {Fore.YELLOW}> {Fore.GREEN}').replace('as > ', f'{Fore.CYAN}AS {Fore.YELLOW}> {Fore.GREEN}').replace('asname > ', f'{Fore.CYAN}AS Name {Fore.YELLOW}> {Fore.GREEN}').replace('mobile > ', f'{Fore.CYAN}Mobile {Fore.YELLOW}> {Fore.GREEN}').replace('proxy', f'{Fore.CYAN}Proxy {Fore.YELLOW}> {Fore.GREEN}').replace('hosting > ', f'{Fore.CYAN}Hosting {Fore.YELLOW}> {Fore.GREEN}'))
